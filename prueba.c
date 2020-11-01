1d8f1e2e862139d82fbbe10564a5b08c89b19826
Keylor Martinez <keylormrodriguez@gmail.com>
2020-10-31 20:47:56 -0600

    segundo commit

--- prueba.c
+++ prueba.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
+    int number1, number2, resultado;
     printf("Esta es una prueba!");
+    number1 = 50;
+    number2 = 5;
+    resultado = number1 + number2;      
+    printf("%d + %d = %d", number1, number2, resultado);
     return 0;
 }
